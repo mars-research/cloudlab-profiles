d20217e2a7c14d91457cd86aa44d65af6047cf79
kosumi <chankocyo@gmail.com>
2023-06-06 03:43:54 -0600

    Clone to dramhit

--- dramhit-setup.sh
+++ dramhit-setup.sh
@@ -118,7 +118,7 @@ clone_dramhit() {
   if [ ! -d ${MOUNT_DIR}/dramhit ]; then
     record_log "Cloning dramhit..."
     pushd ${MOUNT_DIR}
-    git clone https://github.com/KaminariOS/DRAMHiT.git --recursive
+    git clone https://github.com/KaminariOS/DRAMHiT.git --recursive dramhit
     popd;
   else
     record_log "dramhit dir not empty! skipping..."
